3c6000cdce424946d476b88e9c8dd8ebe4476968
imikram <engr.ikram@gmail.com>
2015-08-25 16:47:12 +1000

    adding new database of trackers

--- demo.js
+++ demo.js
@@ -14,7 +14,8 @@ $(function(){
     // Here we are going to add new data to visualize
     //url: 'https://rawgit.com/Ikram/trackv/master/wine-and-cheese.json',
     //url: 'https://rawgit.com/Ikram/trackv/master/trackers-domains.json',
-    url: 'https://rawgit.com/Ikram/trackv/master/trackers-domains-3k.json',
+    //url: 'https://rawgit.com/Ikram/trackv/master/trackers-domains-3k.json',
+    url: 'https://rawgit.com/Ikram/trackv/master/trackers-domains-1k.json',
     //url: 'http://localhost:8080/data/json/wine-and-cheese.json',
     type: 'GET',
     dataType: 'json'
